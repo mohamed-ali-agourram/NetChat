ef6a2d80bf94008cb9cf57587686adcc083cf8b8
Mohamed Ali Agourram <95295909+mohamed-ali-agourram@users.noreply.github.com>
2023-08-31 18:00:42 +0100

    fix api callback URL

--- src/app/helpers/getCurrentUser_F.ts
+++ src/app/helpers/getCurrentUser_F.ts
@@ -4,7 +4,7 @@ export default async function getCurrentUser(email: string) {
     try {
         if (!email) return null
 
-        const user = await axios.get("http://localhost:3000/api/user?email=" + email)
+        const user = await axios.get("/api/user?email=" + email)
 
         if (user) return user.data
 
